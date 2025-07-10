e020eb54d7cc7aad4409c42f0e6dd32bfddeb25c
myrrhex06 <gitlwer210@gmail.com>
2025-07-10 21:14:46 +0900

    Initial commit

---
+++ src/main/java/com/libronote/LibronoteApplication.java
@@ -0,0 +1,13 @@
+package com.libronote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibronoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibronoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libronote/LibronoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libronote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibronoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
